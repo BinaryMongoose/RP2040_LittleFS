a836950cdd61bb61ea065900722f79613c730acb
BinaryMongoose <96893291+BinaryMongoose@users.noreply.github.com>
2024-02-02 11:54:33 -0700

    Finished Tree.ino
    Added comments and everything is probably spelled right.

--- RP2040 Scripts/Tree/Tree.ino
+++ RP2040 Scripts/Tree/Tree.ino
@@ -1,8 +1,9 @@
 /* 
-  This script shows how to work with directories in LittleFS.
-  
-  Loops through each folder looking at what's stored in flash.
+  This is a semi-complicated program, so check out the function diagram in this folder.
+  That's where you'll find most of the information regarding this program. 
 
+  This script shows how to work with directories in LittleFS.
+  Loops through each folder looking at what's stored in flash.
   Prints a handy tree, like if you opened the terminal and typed 'tree'. At least in powershell. 
 
   One thing to note, if you give it a bad directory it won't print anything other than the root. 
@@ -10,9 +11,6 @@
   Though LittleFS does handle these internally, so calling dir.next doesn't return anything. 
 */
 #include <LittleFS.h>
-
-
-File file;
 
 
 void setup() {
@@ -42,34 +40,45 @@ void start_tree(String path) {
   print_tree(path, 0); 
 }
 
+
 // This is a recursive function. Dangerous if left unchecked, but very usefull when employed correctelly. 
 // More info in the function diagram. 
 void print_tree(String dir_name, int level) {
-  // Open whatever directory 
+  // Open the directory with LittleFS
   Dir dir = LittleFS.openDir(dir_name);
 
-  // This actually caused a lot of headahes. The path of the direcotries keep on growing. 
+  // This actually caused a lot of headahes. You must keep note of the 
+  // full path to the directory, or else it won't work. 
   String full_path = dir_name;
 
   while(true) {
-    // No more files.
+    // If there are no more objects, exit.
     if (!dir.next()) {
       break;
     }
 
+    // Indent the layers of the tree.
     for (uint8_t i = 0; i < level; i++) {
       Serial.print("|  ");
     }
 
+
+    // For very file/directory, make a branch.
     Serial.print("|--");
     Serial.print(dir.fileName());
 
+    // Checking if it's a directory.
     if (dir.isDirectory()) {
+      // Print a newline, we want to nest the directories. 
       Serial.println();
+      // Update the path, add the sub-directory we are going into to the directory we started in. 
       full_path = dir_name + "/" + dir.fileName();
+      // Call the function for the sub-directory.
       print_tree(full_path, level + 1);
     } else {
+      // Add a tab to seperate file name and size.
       Serial.print("\t");
+      // Print the file size. 
       Serial.println(dir.fileSize());
     }
   }
